b00f06d55401ce662cb56d532866e0073cf6a7b3
Davide Pulci <davide.pulci@gmail.com>
2022-05-23 20:30:23 +0200

    visualizzati treni con partenza in data odierna

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,20 @@
 </head>
 
 <body>
-    @dump($trains)
+    {{-- @dump($trains) --}}
+    <h1>Treni in partenza oggi</h1>
+    <div>
+        @foreach ($trains as $train)
+            <div style="margin-top: 50px">
+                <p>TRENO: {{ $train->azienda }}</p>
+                <p>STAZIONE DI PARTENZA: {{ $train->stazione_di_partenza }}</p>
+                <p>STAZIONE DI ARRIVO: {{ $train->stazione_di_arrivo }}</p>
+                <p>DATA DI PARTENZA: {{ $train->data_partenza }}</p>
+                <p>ORARIO DI PARTENZA: {{ $train->orario_partenza }}</p>
+                <p>ORARIO DI ARRIVO: {{ $train->orario_arrivo }}</p>
+            </div>
+        @endforeach
+    </div>
 </body>
 
 </html>
